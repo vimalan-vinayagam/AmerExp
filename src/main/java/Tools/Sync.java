fbaa265ab78ece5f85d51135333ec8b7ab1198fd
vimalan-vinayagam <vimalanvinayagam2712@gmail.com>
2025-02-12 19:25:21 +0530

    updated webdriver wait

--- src/main/java/Tools/Sync.java
+++ src/main/java/Tools/Sync.java
@@ -2,9 +2,18 @@ package Tools;
 
 
 import org.openqa.selenium.WebDriver;
-import java.util.concurrent.TimeUnit;
-public class Sync    {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+public class Sync extends BaseClass   {
+    WebDriverWait wait;
+
+    public Sync(){
+        this.wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(10));
+    }
     public  void waitForSeconds(int lngSeconds) {
         try {
             Thread.sleep( lngSeconds);
@@ -23,6 +32,14 @@ public class Sync    {
             System.out.println("Dynamic wait "+e);
         }
 
+    }
+
+    public WebElement waitForElementToVisible(WebElement element){
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public WebElement waitForElementToVisibleClickable(WebElement element){
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
 
